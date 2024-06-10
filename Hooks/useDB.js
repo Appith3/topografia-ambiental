dfc984a0d78f7c2814aad5d7f530c39aae564f86
appith3 <avilesjoel413@gmail.com>
2024-06-10 15:08:43 -0600

    FEAT: update specimen by ID

--- Hooks/useDB.js
+++ Hooks/useDB.js
@@ -1,15 +1,15 @@
 import { useEffect, useState} from 'react'
-import { useSQLiteContext } from 'expo-sqlite';
-import { useStore } from 'zustand';
+import { useSQLiteContext } from 'expo-sqlite'
+import { useStore } from 'zustand'
 
-import { useVegetationStore } from '../store/useVegetationStore';
+import { useVegetationStore } from '../store/useVegetationStore'
 
 const useDB = () => {
 	const [loading, setLoading] = useState(false)
 
-	const db = useSQLiteContext();
+	const db = useSQLiteContext()
 
-	const { addSpecimen } = useStore(useVegetationStore);
+	const { addSpecimen } = useStore(useVegetationStore)
 
 	const initDB = async () => {
 		await db.execAsync(`
@@ -17,12 +17,12 @@ const useDB = () => {
 				id TEXT PRIMARY KEY,
 				classification TEXT,
 				height REAL
-			);
+			)
 			CREATE TABLE IF NOT EXISTS Specimens_Detail (
 				id_specimen TEXT PRIMARY KEY REFERENCES Specimens(id),
 				trunk_diameter REAL,
 				cup_diameter REAL
-			);
+			)
 		`)
 	}
 		
@@ -30,12 +30,12 @@ const useDB = () => {
 		const {id, classification, height, trunk_diameter, cup_diameter} = specimen
 
 		const insertSpecimenStmt = await db.prepareAsync(
-			'INSERT INTO Specimens (id, classification, height) VALUES ($id, $classification, $height);'
-		);
+			'INSERT INTO Specimens (id, classification, height) VALUES ($id, $classification, $height)'
+		)
 
 		const insertSpecimenDetailStmt = await db.prepareAsync(
-			'INSERT INTO Specimens_Detail (id_specimen, trunk_diameter, cup_diameter) VALUES ($id_specimen, $trunk_diameter, $cup_diameter);'
-		);
+			'INSERT INTO Specimens_Detail (id_specimen, trunk_diameter, cup_diameter) VALUES ($id_specimen, $trunk_diameter, $cup_diameter)'
+		)
 
 		try {
 			await insertSpecimenStmt.executeAsync({$id: id, $classification: classification, $height: height})
@@ -52,9 +52,9 @@ const useDB = () => {
 
 		} catch (error) {
 			if (error.message.includes('database is locked')) {
-        console.error("Database locked");
+        console.error("Database locked")
       } else {
-        throw error; 
+        throw error 
       }
 		}
 		finally {
@@ -64,36 +64,131 @@ const useDB = () => {
 
 	const getAllSpecimens = async () => {
 		const getSpecimensStmt = await db.prepareAsync(
-			'SELECT * FROM Specimens s LEFT JOIN Specimens_Detail sd ON s.id = sd.id_specimen;'
+			'SELECT * FROM Specimens s LEFT JOIN Specimens_Detail sd ON s.id = sd.id_specimen'
 		)
 
 		try {
-			setLoading(true); 
+			setLoading(true) 
 	
-			const result = await getSpecimensStmt.executeAsync();
-			const allRows = await result.getAllAsync();
+			const result = await getSpecimensStmt.executeAsync()
+			const allRows = await result.getAllAsync()
 	
 			for(specimen of allRows) {
 				addSpecimen(specimen)
 			}
 	
-			setLoading(false); 
+			setLoading(false) 
 		} catch (error) {
-			console.error('Error fetching specimens:', error); 
+			console.error('Error fetching specimens:', error) 
 		} finally {
-			await getSpecimensStmt.finalizeAsync(); 
+			await getSpecimensStmt.finalizeAsync() 
+		}
+	}
+
+	const getSpecimenById = async (specimenId) => {
+		const getByIdStmt = await db.prepareAsync(
+			'SELECT * FROM Specimens s LEFT JOIN Specimens_Detail sd ON s.id = sd.id_specimen WHERE s.id = $specimenId'
+		)
+	
+		try {
+			const result = await getByIdStmt.executeAsync({ $specimenId: specimenId })
+			const specimenRow = await result.getFirstAsync()
+	
+			if (!specimenRow) {
+				console.warn(`Specimen with ID ${specimenId} not found`)
+				return null
+			}
+	
+			const specimenData = {
+				classification: specimenRow.classification,
+				height: specimenRow.height,
+				trunk_diameter: specimenRow.trunk_diameter || null,
+				cup_diameter: specimenRow.cup_diameter || null,
+				}
+	
+			return specimenData
+		} catch (error) {
+			console.error('Error getting specimen by ID:', error)
+			return null
+		} finally {
+			await getByIdStmt.finalizeAsync()
+		}
+	}
+
+	const updateSpecimenClassification = async (specimenId, newClassification) => {
+		const updateStmt = await db.prepareAsync(
+			'UPDATE Specimens SET classification = $newClassification WHERE id = $specimenId'
+		)
+	
+		try {
+			await updateStmt.executeAsync({ $specimenId: specimenId, $newClassification: newClassification })
+		} catch (error) {
+			console.error('Error updating specimen classification:', error)
+		} finally {
+			await updateStmt.finalizeAsync()
+		}
+	}
+
+	const updateSpecimenHeight = async (specimenId, newHeight) => {
+		const updateStmt = await db.prepareAsync(
+			'UPDATE Specimens SET height = $newHeight WHERE id = $specimenId'
+		)
+	
+		try {
+			await updateStmt.executeAsync({ $specimenId: specimenId, $newHeight: newHeight })
+		} catch (error) {
+			console.error('Error updating specimen height:', error)
+		} finally {
+			await updateStmt.finalizeAsync()
+		}
+	}
+
+	const updateSpecimenTrunkDiameter = async (specimenId, newTrunkDiameter) => {
+		if (!newTrunkDiameter) return // No update if new value is null/undefined
+	
+		const updateStmt = await db.prepareAsync(
+			'UPDATE Specimens_Detail SET trunk_diameter = $newTrunkDiameter WHERE id_specimen = $specimenId'
+		)
+	
+		try {
+			await updateStmt.executeAsync({ $specimenId: specimenId, $newTrunkDiameter: newTrunkDiameter })
+		} catch (error) {
+			console.error('Error updating specimen trunk diameter:', error)
+		} finally {
+			await updateStmt.finalizeAsync()
+		}
+	}
+
+	const updateSpecimenCupDiameter = async (specimenId, newCupDiameter) => {
+		if (!newCupDiameter) return // No update if new value is null/undefined
+	
+		const updateStmt = await db.prepareAsync(
+			'UPDATE Specimens_Detail SET cup_diameter = $newCupDiameter WHERE id_specimen = $specimenId'
+		)
+	
+		try {
+			await updateStmt.executeAsync({ $specimenId: specimenId, $newCupDiameter: newCupDiameter })
+		} catch (error) {
+			console.error('Error updating specimen cup diameter:', error)
+		} finally {
+			await updateStmt.finalizeAsync()
 		}
 	}
 
 	useEffect(() => {
 		initDB()
-	}, []);
+	}, [])
 	
 	return {
 		createSpecimen,
 		getAllSpecimens,
+		getSpecimenById,
+		updateSpecimenClassification,
+		updateSpecimenHeight,
+		updateSpecimenTrunkDiameter,
+		updateSpecimenCupDiameter,
 		loading
 	}
 }
 
-export default useDB;
+export default useDB
---
+++ screens/VegetationDetailScreen.jsx
@@ -0,0 +1,149 @@
+import { StyleSheet, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { Button, TextInput } from 'react-native-paper'
+
+import Topbar from '../components/Topbar'
+import InputSelect from '../components/InputSelect'
+import useDB from '../Hooks/useDB'
+
+const VegetationDetailScreen = ( props ) => {
+
+	const {
+		navigation,
+		route
+	} = props
+
+	const { id } = route.params
+
+	const {
+		getSpecimenById,
+		updateSpecimenClassification,
+		updateSpecimenHeight,
+		updateSpecimenTrunkDiameter,
+		updateSpecimenCupDiameter
+	} = useDB()
+
+	const [specimen, setSpecimen] = useState({
+		classification: '',
+		height: '',
+		trunk_diameter: '',
+		cup_diameter: ''
+	})
+
+	const loadSpecimen = async (specimenId) => {
+    const fetchedSpecimen = await getSpecimenById(specimenId)
+		console.log('fetchedSpecimen: ', fetchedSpecimen);
+    setSpecimen(fetchedSpecimen)
+  }
+
+	const handleEditPress = async () => {
+    await updateSpecimenClassification(id, specimen.classification)
+    await updateSpecimenHeight(id, parseFloat(specimen.height)) 
+    if (specimen.trunk_diameter !== '') { 
+      await updateSpecimenTrunkDiameter(id, parseFloat(specimen.trunk_diameter))
+    }
+    if (specimen.cup_diameter !== '') {
+      await updateSpecimenCupDiameter(id, parseFloat(specimen.cup_diameter))
+    }
+  }
+
+	useEffect(() => {
+		loadSpecimen(id)
+	}, [])
+
+	return (
+		<View style={styles.container}>
+			<Topbar title={`Editar ejemplar ${id}`} whitBackAction onBack={() => {navigation.goBack()}}/>
+			<View style={styles.form}>
+				<InputSelect 
+					options={[
+						{
+							label: 'Selecciona una clasificación',
+							value: 'default'
+						},
+						{
+							label: 'Árbol',
+							value: 'árbol'
+						},
+						{
+							label: 'Arbusto',
+							value: 'arbusto'
+						},
+						{
+							label: 'Palma',
+							value: 'palma'
+						},
+						{
+							label: 'Suculenta',
+							value: 'suculenta'
+						},
+						{
+							label: 'Cactácea',
+							value: 'cactácea'
+						},
+					]}
+					selectedValue={specimen.classification}
+					onValueChange={(classification) => {
+						setSpecimen({
+							...specimen,
+							classification,
+						})
+					}}
+				/>
+				<TextInput
+					label="Altura"
+					value={specimen.height?.toString()}
+					onChangeText={height => setSpecimen({
+						...specimen,
+						height
+					})}
+					keyboardType='numeric'
+				/>
+				<TextInput
+					label="Diámetro de la copa"
+					value={specimen.cup_diameter?.toString()}
+					onChangeText={cup_diameter => setSpecimen({
+						...specimen,
+						cup_diameter
+					})}
+					keyboardType='numeric'
+				/>
+				<TextInput
+					label="Diámetro del tronco"
+					value={specimen.trunk_diameter?.toString()}
+					onChangeText={trunk_diameter => setSpecimen({
+						...specimen,
+						trunk_diameter
+					})}
+					keyboardType='numeric'
+				/>
+
+				<View style={styles.formControls}>
+					<Button mode='contained' onPress={handleEditPress}>
+						Editar ejemplar
+					</Button>
+				</View>
+			</View>
+		</View>
+	)
+}
+
+export default VegetationDetailScreen
+
+const styles = StyleSheet.create({
+	container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+	form: {
+		flex: 1,
+		justifyContent: 'center',
+		margin: 16,
+		gap: 18,
+	},
+	formControls: {
+		marginTop: 42,
+		gap: 20,
+	}
+})
